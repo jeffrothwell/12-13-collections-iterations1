0d2e1a0286d5124e2056024f18c0d70d99dbf91a
jeffrothwell <rothwelljeff@gmail.com>
2017-12-13 14:33:26 -0500

    complete ex1

--- coll-and-it.rb
+++ coll-and-it.rb
@@ -2,7 +2,7 @@ fav_colours = ["blue", "red", "green", "purple"]
 ages = [33, 29, 32, 31]
 coin_flips = ["heads", "tails", "tails", "heads", "tails"]
 fav_artists = ["Phish", "Grateful Dead", "Umphrey's McGee"]
-fav_colours = [:blue, :red, :green, :purple]
+fav_colours_labels = [:blue, :red, :green, :purple]
 
 word_defs = {finite: "a number with a defined value in the real number set",
   heady: "requiring one to think deeply",
@@ -11,3 +11,12 @@ word_defs = {finite: "a number with a defined value in the real number set",
 fav_movies = {Pulp_Fiction: 1994, Rushmore: 1998, Raising_Arizona: 1987}
 cities = {Toronto: 2809000, London: 8788000, Mumbai: 18410000}
 names_ages = {Jeff: 33, Moya: 29, Adam: 32, Dan: 33}
+
+#exercise 1
+
+puts coin_flips
+puts fav_colours[0]
+puts ages.sort
+ages << 0
+puts ages
+puts "Rushmore was released in #{fav_movies[:Rushmore]}"
