d2c1c0738c52fe6b3e77effe263a5d5b09ef0500
jeffrothwell <rothwelljeff@gmail.com>
2017-12-13 13:58:12 -0500

    create arrays and hashes for exercises

---
+++ coll-and-it.rb
@@ -0,0 +1,13 @@
+fav_colours = ["blue", "red", "green", "purple"]
+ages = [33, 29, 32, 31]
+coin_flips = ["heads", "tails", "tails", "heads", "tails"]
+fav_artists = ["Phish", "Grateful Dead", "Umphrey's McGee"]
+fav_colours = [:blue, :red, :green, :purple]
+
+word_defs = {finite: "a number with a defined value in the real number set",
+  heady: "requiring one to think deeply",
+  chomper: "one who grinds one's teeth"
+}
+fav_movies = {Pulp_Fiction: 1994, Rushmore: 1998, Raising_Arizona: 1987}
+cities = {Toronto: 2809000, London: 8788000, Mumbai: 18410000}
+names_ages = {Jeff: 33, Moya: 29, Adam: 32, Dan: 33}
