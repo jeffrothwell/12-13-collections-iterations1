9a604532204aa725c198ad3fe6110c1c46d94123
jeffrothwell <rothwelljeff@gmail.com>
2017-12-14 14:28:35 -0500

    complete ex 10

--- exercise10.rb
+++ exercise10.rb
@@ -3,3 +3,26 @@ students = {cohort1: 34, cohort2: 42, cohort3: 22}
 students.each do |cohort, num|
   puts "#{cohort}: #{num} students"
 end
+
+students[:cohort4] = 43
+
+puts students.keys
+
+more_students = {}
+
+students.each do |cohort, num|
+  more_students[cohort] = num * 1.05
+end
+
+puts more_students
+
+students.delete(:cohort2)
+puts students
+
+total_students = 0
+
+students.each do |cohort, num|
+  total_students += num
+end
+
+puts "There are #{total_students} across all cohorts"
